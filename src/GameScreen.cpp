b2b53dad66114a759766f20c427f7b888ee64948
Louis <louisvdw99@gmail.com>
2018-11-22 19:24:02 +0200

    Updated comments in GameScreen.cpp

--- src/GameScreen.cpp
+++ src/GameScreen.cpp
@@ -4,22 +4,22 @@ GameScreen::GameScreen()
 {
     //ctor
 }
-
+//Init the scren
 void GameScreen::init()
 {
 
 }
-
+//Handle input 
 void GameScreen::handleInput(double deltatime)
 {
 
 }
-
+//Run the main loop
 void GameScreen::loop(sf::RenderWindow* window)
 {
 
 }
-
+//Cleanup before exit
 void GameScreen::clean()
 {
 
